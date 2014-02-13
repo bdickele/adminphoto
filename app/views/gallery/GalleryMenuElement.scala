4d2a031773ebd244f10d30d082645d68e2b63917
Bertrand DICKELE <bdickele@gmail.com>
2014-02-13 21:23:38 +0100

    Final changes : theme, layout, nav

---
+++ app/views/gallery/GalleryMenuElement.scala
@@ -0,0 +1,13 @@
+package views.gallery
+
+/**
+ * Created by bdickele
+ * Date: 2/13/14
+ */
+object GalleryMenuElement extends Enumeration {
+
+  val GalleryForm = Value
+  val PicList = Value
+  val PicSelection = Value
+
+}
