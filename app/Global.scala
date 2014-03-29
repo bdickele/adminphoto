3e948cf1fdaf489bf6249a006a90811c060ce0f0
Bertrand DICKELE <bdickele@gmail.com>
2014-03-29 10:46:35 +0100

    Misc : gallery backup

---
+++ app/Global.scala
@@ -0,0 +1,41 @@
+import play.api.{Logger, Application, GlobalSettings}
+import scala.Some
+
+/**
+ * Overriding GlobalSettings to customize error pages for instance
+ * Date: 3/29/14
+ */
+object Global extends GlobalSettings {
+
+  override def onStart(app: Application) {
+    val databaseMessage =
+      app.configuration.getString("mongodb.uri") match {
+        case None => "Damn, could not find database configuration"
+        case Some(dbUri) =>
+          if (dbUri.contains("localhost")) "You're running on a LOCAL database"
+          else "Keep in mind you're running on a REMOTE database"
+      }
+    prettyLog(databaseMessage)
+  }
+
+  /*
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(
+      views.html.notFound(request.path)
+    ))
+  }
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    Future.successful(BadRequest(
+      views.html.badRequest(ex.getMessage)
+    ))
+  }
+  */
+
+  def prettyLog(messages: String*) = {
+    Logger.info("****************************************************")
+    messages.foreach(Logger.info(_))
+    Logger.info("****************************************************")
+  }
+}
+
